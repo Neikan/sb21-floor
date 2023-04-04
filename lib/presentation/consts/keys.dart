7bf25a62a1181f476782bb86349cd048e818c3b8
Neikan <sladkov.e.m@outlook.com>
2023-04-04 15:34:39 +0300

    feat(presentation): add keys to consts

---
+++ lib/presentation/consts/keys.dart
@@ -0,0 +1,8 @@
+const String keyTitle = 'title';
+const String keyContent = 'content';
+
+const String keyName = 'name';
+const String keySurname = 'surname';
+const String keyAge = 'age';
+const String keyPhone = 'phone';
+const String keyCard = 'card';
