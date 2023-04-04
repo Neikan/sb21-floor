17000ad3698aef157d5c51148cd7a294dd5eaa96
Neikan <sladkov.e.m@outlook.com>
2023-04-04 18:02:30 +0300

    feat(data): add repository

---
+++ lib/data/repositories/repository_persons/repository_persons.dart
@@ -0,0 +1,18 @@
+// Project imports:
+import 'package:app_floor/data/models/app_person/app_person.dart';
+
+abstract class RepositoryPersons {
+  const RepositoryPersons();
+
+  Future<List<AppPerson>> get persons;
+  Future<Map<String, String>> get cards;
+  int get maxId;
+
+  Future<void> init();
+
+  Future<void> add(AppPerson person, String card);
+
+  Future<void> update(AppPerson person, String card);
+
+  Future<void> remove(int id);
+}
